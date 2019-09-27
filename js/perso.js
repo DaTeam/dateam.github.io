50fcaef1ab6f70ff6de4d8ee128a12236c434adf
Guillaume Spera <guillaume.spera@gmail.com>
2019-09-27 13:06:47 +0200

    remove old JS

--- js/perso.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	$(window).scroll(function(){
-		posScroll = $(document).scrollTop();
-		if(posScroll >=10) 
-			$('body').addClass('scroll');
-		else
-			$('body').removeClass('scroll');
-	});
-});
